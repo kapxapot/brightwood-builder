f484372f23856a42f41f2150e045ac9b53d5168f
Sergey Atroshchenko <kapxapot@gmail.com>
2023-10-16 16:15:34 +0400

    Keys and borders

--- src/components/node-parts/node-text.tsx
+++ src/components/node-parts/node-text.tsx
@@ -14,8 +14,8 @@ export default function NodeText({ text }: Props) {
 
   return (
     <div className="text-sm mt-2 space-y-2">
-      {lines.map(line => (
-        <p className="border border-black border-opacity-20 rounded-lg border-dashed bg-white bg-opacity-50 px-2 py-1">{line}</p>
+      {lines.map((line, index) => (
+        <p key={index} className="border border-black border-opacity-20 rounded-lg border-dashed bg-white bg-opacity-50 px-2 py-1">{line}</p>
       ))}
     </div>
   );
