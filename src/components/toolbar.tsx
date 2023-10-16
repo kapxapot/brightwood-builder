01750a052b31bf7d97e117573e2c31acb2f0a160
Sergey Atroshchenko <kapxapot@gmail.com>
2023-10-16 16:07:31 +0400

    Icons

--- src/components/toolbar.tsx
+++ src/components/toolbar.tsx
@@ -2,12 +2,12 @@ import ToolbarBlock from "./toolbar-block";
 
 export default function Toolbar() {
   return (
-    <aside className="w-24 bg-gray-200 p-2 border-r border-r-gray-300 space-y-3">
+    <aside className="w-32 bg-gray-200 p-2 border-r border-r-gray-300 space-y-3">
       <div>Drag 👉</div>
-      <ToolbarBlock type="action" label="Action" />
-      <ToolbarBlock type="skip" label="Skip" />
-      <ToolbarBlock type="redirect" label="Redirect" />
-      <ToolbarBlock type="finish" label="Finish" />
+      <ToolbarBlock type="action" label="⚡ Action" />
+      <ToolbarBlock type="skip" label="🚀 Skip" />
+      <ToolbarBlock type="redirect" label="🔱 Redirect" />
+      <ToolbarBlock type="finish" label="⛔ Finish" />
     </aside>
   );
 };
