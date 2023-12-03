94b52eea78043d527ac86069baed553c38295905
Sergey Atroshchenko <kapxapot@gmail.com>
2023-12-03 12:58:29 +0400

    Node text renders HTML

--- src/components/node-parts/node-text.tsx
+++ src/components/node-parts/node-text.tsx
@@ -15,7 +15,7 @@ export default function NodeText({ text }: Props) {
   return (
     <div className="text-sm mt-2 space-y-2">
       {lines.map((line, index) => (
-        <p key={index} className="border border-black border-opacity-20 rounded-lg border-dashed bg-white bg-opacity-50 px-2 py-1">{line}</p>
+        <p key={index} className="border border-black border-opacity-20 rounded-lg border-dashed bg-white bg-opacity-50 px-2 py-1" dangerouslySetInnerHTML={{ __html: line }}></p>
       ))}
     </div>
   );
