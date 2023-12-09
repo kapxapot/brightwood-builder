52ebe57e74828633db85305522d8cc58a696bee6
Sergey Atroshchenko <kapxapot@gmail.com>
2023-12-09 16:20:16 +0300

    Redirect links removal

--- src/components/nodes/action-node.tsx
+++ src/components/nodes/action-node.tsx
@@ -52,7 +52,10 @@ const ActionNode = memo(function ActionNode({ data, selected }: Props) {
         {data.actions.map((action, index) => (
           <div className="relative group text-sm bg-gradient-to-r from-transparent to-green-300 py-1 -mr-2" key={index}>
             <div>
-              <div>⚡ <span className={`${!action.label.length && "opacity-30"}`}>{action.label || `Action ${index + 1}`}</span>&nbsp;<NodeRef id={action.id} /></div>
+              <div>
+                ⚡ <span className={`mr-1 ${!action.label.length && "opacity-30"}`}>{action.label || `Action ${index + 1}`}</span>
+                <NodeRef id={action.id} />
+              </div>
               <Handle id={String(index)} type="source" position={Position.Right} className="bg-slate-600" isConnectable={true} />
             </div>
             <div className="absolute right-2 inset-y-0 space-x-1 hidden group-hover:block">
@@ -61,7 +64,9 @@ const ActionNode = memo(function ActionNode({ data, selected }: Props) {
             </div>
           </div>
         ))}
-        <Button onClick={addAction}>Add action ⚡</Button>
+        <Button onClick={addAction}>
+          Add action ⚡
+        </Button>
       </div>
     </NodeShell>
   );
--- src/components/nodes/redirect-node.tsx
+++ src/components/nodes/redirect-node.tsx
@@ -4,6 +4,7 @@ import type { Link, RedirectStoryNode } from '../../entities/story-node';
 import { Colors } from '../../lib/constants';
 import NodeShell from '../node-parts/node-shell';
 import NodeRef from '../node-parts/node-ref';
+import Button from '../core/button';
 
 interface Props {
   data: RedirectStoryNode;
@@ -20,6 +21,23 @@ const RedirectNode = memo(function RedirectNode({ data, selected }: Props) {
       ]
     });
   }
+
+  const editLink = (index: number) => {
+    console.log(`Editing link ${index}...`);
+  };
+
+  const deleteLink = (index: number) => {
+    data.onChange?.(
+      {
+        ...data,
+        links: data.links.toSpliced(index, 1)
+      },
+      {
+        type: "handleRemoved",
+        handle: String(index)
+      }
+    );
+  };
 
   function weightStr(link: Link): string {
     const weight = Math.max(1, link.weight ?? 1);
@@ -40,22 +58,30 @@ const RedirectNode = memo(function RedirectNode({ data, selected }: Props) {
       data={data}
       label="Redirect"
     >
-      {data.links.map((link, index) => (
-        <div className="mt-2 text-sm bg-gradient-to-r from-transparent to-yellow-300 py-1 relative -mr-2" key={index}>
-          <div>
-            <span className="mr-1">{weightStr(link)}</span>
-            {link.condition && (
-              <span className="mr-1">
-                <span className="italic">if:</span> {link.condition}
-              </span>
-            )}
-            <NodeRef id={link.id} />
+      <div className="mt-2 space-y-2">
+        {data.links.map((link, index) => (
+          <div className="relative group text-sm bg-gradient-to-r from-transparent to-yellow-300 py-1 -mr-2" key={index}>
+            <div>
+              <div>
+                <span className="mr-1">{weightStr(link)}</span>
+                {link.condition && (
+                  <span className="mr-1">
+                    <span className="italic">if:</span> {link.condition}
+                  </span>
+                )}
+                <NodeRef id={link.id} />
+              </div>
+              <Handle id={String(index)} type="source" position={Position.Right} className="bg-slate-600" isConnectable={true} />
+            </div>
+            <div className="absolute right-2 inset-y-0 space-x-1 hidden group-hover:block">
+              <Button onClick={() => editLink(index)}>Edit</Button>
+              <Button onClick={() => deleteLink(index)}>Delete</Button>
+            </div>
           </div>
-          <Handle id={String(index)} type="source" position={Position.Right} className="bg-slate-600" isConnectable={true} />
-        </div>
-      ))}
-      <div className="mt-2">
-        <button onClick={addLink} className="border border-slate-400 px-2 pt-0.5 pb-1 rounded-lg text-sm bg-slate-50 hover:bg-white">Add link 🎲</button>
+        ))}
+        <Button onClick={addLink}>
+          Add link 🎲
+        </Button>
       </div>
     </NodeShell>
   );
