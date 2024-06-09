a53274e37dec12c16b21ada648b5c583d470b77a
Sergey Atroshchenko <kapxapot@gmail.com>
2024-06-09 12:29:35 +0400

    UI unification

--- src/components/node-parts/node-title.tsx
+++ src/components/node-parts/node-title.tsx
@@ -7,7 +7,7 @@ export default function NodeTitle({ id, label }: Props) {
   return (
     <div className="space-x-2 custom-drag-handle cursor-grab">
       {id > 0 && <span className="border border-gray-500 px-2 rounded-xl bg-gray-50">{id}</span>}
-      <span className="font-bold">{label}</span>
+      <span className="font-semibold">{label}</span>
     </div>
   )
 }
