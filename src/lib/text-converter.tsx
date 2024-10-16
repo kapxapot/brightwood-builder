74b61aca80cea760c2ab4e7d5ca5ad17e350514e
Sergey Atroshchenko <kapxapot@gmail.com>
2024-10-16 13:31:37 +0400

    Adds semantic highlight

--- src/lib/text-converter.tsx
+++ src/lib/text-converter.tsx
@@ -1,6 +1,26 @@
 export function convertText(text: string): string {
-  text = text.replace("<tg-spoiler>", "&lt;tg-spoiler&gt;");
-  text = text.replace("</tg-spoiler>", "&lt;/tg-spoiler&gt;");
+  text = text.replaceAll("<tg-spoiler>", green("&lt;tg-spoiler&gt;"));
+  text = text.replaceAll("</tg-spoiler>", green("&lt;/tg-spoiler&gt;"));
+
+  text = curlyBraces(text);
 
   return text;
 }
+
+const green = (content: string) => `<span class="text-green-500 font-semibold">${content}</span>`;
+const blue = (content: string) => `<span class="text-blue-700">${content}</span>`;
+const yellow = (content: string) => `<span class="text-yellow-600">${content}</span>`;
+
+const curlyBraces = (text: string) =>
+  text.replaceAll(/{{(?:([a-z]+):)?(.+?)}}/g, (_match: string, p1: string, p2: string) => {
+    const content = p2
+      .split("|")
+      .map(p => blue(p))
+      .join(green("|"));
+
+    const prefix = p1
+      ? yellow(p1) + green(":")
+      : "";
+
+    return green("{{") + prefix + content + green("}}");
+  });
